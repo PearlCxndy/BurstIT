90eb348e1e0c6a225f38da4788e2f1e59ceafe0d
Astryd Park <artreadcode@gmail.com>
2025-01-26 13:02:08 +0000

    arghhhhhh

--- Assets/Scripts/AnimalSpawner.cs
+++ Assets/Scripts/AnimalSpawner.cs
@@ -5,55 +5,17 @@ using UnityEngine;
 
 public class AnimalSpawner : MonoBehaviour
 {
-    public GameObject[] objectsToSpawn;
-    public float spawnRadius = 10.0f; // Default value
-    public int numberOfObjects = 10; // Default value
+    public GameObject animal;
 
-    public bool randomOrientation = false;
-    public bool orientToSurface = false;
-
-    void Start()
+    public void Trigger()
     {
-        for (int i = 0; i < numberOfObjects; i++)
+        if (animal.activeInHierarchy == false)
         {
-            // What we will spawn
-            GameObject objectToSpawn = objectsToSpawn[UnityEngine.Random.Range(0, objectsToSpawn.Length)];
-
-            // Vector2 spawnPositionV2 = UnityEngine.Random.insideUnitCircle * spawnRadius;
-            Vector3 spawnPositionV2 = UnityEngine.Random.onUnitSphere * spawnRadius;
-
-            // Vector3 spawnPosition = new Vector3(spawnPositionV2.x, 0.0f, spawnPositionV2.y);
-            Vector3 spawnPosition = new Vector3(spawnPositionV2.x, spawnPositionV2.y, spawnPositionV2.z);
-
-            Vector3 transformOffsetSpawnPosition = transform.position + spawnPosition;
-
-            // Quaternion.identity
-
-            RaycastHit hit;
-            if (Physics.Raycast(transformOffsetSpawnPosition, Vector3.down, out hit))
-            {
-                Vector3 finalSpawnPosition = hit.point;
-
-                Quaternion orientation;
-
-                if (randomOrientation)
-                {
-                    orientation = UnityEngine.Random.rotation;
-                }
-                else if (orientToSurface)
-                {
-                    orientation = Quaternion.LookRotation(hit.normal);
-                }
-                else
-                {
-                    orientation = objectToSpawn.transform.rotation;
-                }
-
-
-                Instantiate(objectToSpawn, finalSpawnPosition, orientation);
-                Debug.Log("Spawned " + objectToSpawn.name + " at " + finalSpawnPosition);
-            }
-
+            animal.SetActive(true);
+        }
+        else
+        {
+            animal.SetActive(false);
         }
     }
 }
